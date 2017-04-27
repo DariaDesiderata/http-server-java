2e4586af4fdb4b847b1911bb97a75aae83d3b029
DariaDesiderata <daria.calwell@gmail.com>
2017-04-27 14:53:31 -0600

    seeded db

---
+++ src/main/java/src/main/java/db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO book (title, author, url)
+VALUES
+('You Don''t Know JS: Up & Going', 'Kyle Simpson', 'http://shop.oreilly.com/product/0636920039303/ViewLarger.do?sortby=bestSellers'),
+('RESTful Web Clients', 'Mike Amundsen', 'http://shop.oreilly.com/product/0636920037958/ViewLarger.do?sortby=bestSellers'),
+('Raspberry Pi Cookbook','Simon Monk', 'http://shop.oreilly.com/product/0636920045182/ViewLarger.do?sortby=bestSellers'),
+('Introduction to Machine Learning with Python', 'Andreas C. Müller, Sarah Guido', 'http://shop.oreilly.com/product/0636920030515/ViewLarger.do?sortby=bestSellers'),
+('You Don''t Know JS: Async & Performance', 'Kyle Simpson', 'http://shop.oreilly.com/product/0636920033752/ViewLarger.do?sortby=bestSellers')
